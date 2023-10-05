098377695f3394b535dc16f2dc6054d71d5fb6e2
b-juu <erichassett4@gmail.com>
2023-10-05 10:49:21 -0400

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace MethodOverloading
+﻿using System;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); 
+            string result = program.Add(1, 1, true);
+            Console.WriteLine($"{result}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public string Add(int a, int b, bool includeDollars)
+        {
+            if (includeDollars)
+            {
+                string plural = (a + b == 1) ? "" : "s";
+                return $"{a + b} dollar{plural}";
+            }
+            else
+            {
+                return (a + b).ToString();
+            }
         }
     }
 }
